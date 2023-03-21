c9657b807515b0dd4b5f02ffd7849fd5a841bf81
AnLee-dev <levanandtb05111999@gmail.com>
2023-03-21 17:32:08 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+'use strict'
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan("combined"));
+
+//template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+//router
+app.get('/', (req, res) => {
+  res.render('home');
+});
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port);
